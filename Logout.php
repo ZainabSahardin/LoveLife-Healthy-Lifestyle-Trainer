ecaca52a0f3e6310c062b3c989d0981a34eaef3d
nurulshafiqa <95677589+nurulshafiqa@users.noreply.github.com>
2022-02-09 13:01:01 +0800

    Delete Logout.php

--- Logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();    // start a resume session
-session_unset();    // remove the session
-session_destroy();  // destroy all the sessions
-
-header("location: index.php");
-exit();
